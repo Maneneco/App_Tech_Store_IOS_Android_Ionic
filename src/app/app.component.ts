3ac8fb63740065c0988380bf3c9a4809c14abc47
Carlos <carloscz@ipvc.pt>
2021-07-01 22:38:56 +0100

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
@@ -8,6 +8,9 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  logged = false;
+  showsearchbar = false;
+  @ViewChild('topbar') pRef: ElementRef;
   constructor(private menu: MenuController, private router: Router) {}
 
   menuteste() {
@@ -29,8 +32,37 @@ export class AppComponent {
     this.menu.enable(true, 'menu-profile');
     this.menu.open('menu-profile');
   }
-
   menumobile() {
     this.router.navigate(['/mobile']);
+  }
+  menucreate() {
+    this.menu.enable(true, 'menu-create');
+    this.menu.open('menu-create');
+  }
+  menuloginemail() {
+    this.menu.enable(true, 'menu-login-email');
+    this.menu.open('menu-login-email');
+  }
+  menucart() {
+    this.menu.enable(true, 'menu-cart');
+    this.menu.open('menu-cart');
+  }
+  login() {
+    this.logged = true;
+    localStorage.setItem('logged', String(this.logged));
+  }
+  logout() {
+    this.logged = false;
+    localStorage.setItem('logged', String(this.logged));
+  }
+
+  togglesearchbar() {
+    if (this.showsearchbar == false) {
+      this.showsearchbar = true;
+      document.getElementById('topbar').style.height = '110px';
+    } else {
+      this.showsearchbar = false;
+      document.getElementById('topbar').style.height = '64px';
+    }
   }
 }
